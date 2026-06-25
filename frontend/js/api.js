4746ec642e0d73ee79b44fb8cbb365b267770ec9
Pranav R <pranavkundapura06@gmail.com>
2026-06-25 16:03:08 +0530

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://exam-system-2k9t.onrender.com";
 
 function showLoading() {
     const spinner = document.getElementById('loading-spinner');
